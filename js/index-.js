cd9a4afc312862b7f75048ba5e1cf57c7ed189c5
mykola-B <prpl.ha@gmail.com>
2015-06-28 18:52:42 +0300

    Main functionality

--- js/index-.js
+++ js/index-.js
@@ -1,6 +1,6 @@
-(function mainInteraction() {
+$(function onLoad() {
   'use strict';
-
+  var userStorage = {};
   function extend(Child, Parent) {
     var F = function ext() {};
     F.prototype = Parent.prototype;
@@ -17,33 +17,81 @@
    *  ------Model-------
    *
    */
-  function User() {
-    this.name = name;
-    this.status = status;
-    this.phone = phone;
-    this.id = id;
+  function User(user) {
+    this.name = user.name;
+    this.status = user.status;
+    this.phone = user.phone;
+    this.id = user.id;
   }
-  function UserStorage() {
-
-  }
-  UserStorage.prototype.usersListParser = function (data) {
-    var users;
-     users = _.map(data, function statusSetter(prop, key, ) {
-       var user = _.create(User.prototype, us);
-    })
+  userStorage.controlRouter = function (usersList) {
+  listConstructor(usersList);
+  domEventListeners();
+  };
+  userStorage._setLocalStorage = function(users, sequence) {
+    localStorage.setItem('users', JSON.stringify(users));
+    localStorage.setItem('sequence', JSON.stringify(sequence));
+    userStorage.controlRouter(users);
+  };
+  // Return array with two objects: user list and user sequence from localStorage
+  userStorage.getLocalStorage = function() {
+    var u;
+    var s;
+    u = localStorage.getItem('users');
+    s = localStorage.getItem('sequence');
+    return [JSON.parse(u), JSON.parse(s)];
+  };
+  userStorage.clearLocalStorage = function () {
+    localStorage.removeItem('users');
+    localStorage.removeItem('sequence');
+  };
+  userStorage.userListParser = function(data) {
+    if (localStorage.getItem('users')) {
+      var userList = localStorage.getItem('users');
+      this.users = userList;
+      userStorage.controlRouter(userList);
+    } else {
+      this.users = _.map(data, function statusSetter(us) {
+        // Создаем объект из конструктора User, определяем ему полученные свойства
+        return _.create(User.prototype, us);
+      });
+    }
+    userStorage._setLocalStorage(this.users, data);
   };
 // -------View-------
-  function domConstructor (users, callback) {
-
+  function listConstructor (data) {
+    function userSort(user) {
+      function liCreator(role) {
+        var $li = $('<li>').html('<h2>' + user.name + '</h2><h3>' + user.phone + '</h3>');
+        $(role).append($li);
+      }
+      if (user.status === 'active') {
+        liCreator('.active ul');
+        return;
+      }
+      if (user.status === 'redcard') {
+        liCreator('.redcard ul');
+        return;
+      }
+      if (user.status === 'removed') {
+        liCreator('.removed ul');
+      }
+    }
+    // Here we parse our associate massive
+    _.map(data, userSort);
+    /* Old version for (var i = 0; i < data.length; i++) {
+      userSort(data[i]);
+    }*/
   }
 // -------Controller--------
-  function replaceListener (selector, callback) {
+  function domEventListeners() {
+    function replaceListener (selector, callback) {
 
-  }
-  function sortListener(selector) {
+    }
+    function sortListener(selector) {
 
+    }
   }
 // Action!
-  $(function onLoad() {
-    });
-})();
+    userStorage.clearLocalStorage();
+    $.get(window.url, null, userStorage.userListParser, 'json');
+});
